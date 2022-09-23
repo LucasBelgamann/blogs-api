1391835d7373ec8cbf0b788d973c46a5b146f23e
LucasBelgamann <lucasbelgamann@hotmail.com>
2022-09-23 16:25:26 -0300

    req 1 completo

---
+++ src/migrations/20220923190733-blog_posts.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('blog_posts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      user_id: {
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        references: { model: 'users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      published: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface) => {
+    return queryInterface.dropTable('blog_posts');
+  }
+};
---
+++ src/migrations/20220923191622-PostCategories.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('posts_categories', {
+      post_id: {
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        references: { model: 'blog_posts', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      category_id: {
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        references: { model: 'categories', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+    });
+  },
+
+  down: async (queryInterface) => {
+    return queryInterface.dropTable('posts_categories')
+  }
+};
